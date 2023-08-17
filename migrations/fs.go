af899db4207c0c2030803dce5b3f1a4a1450a816
ayushthe1 <ayushsharmaa101@gmail.com>
2023-08-17 20:14:33 +0530

    embed the sql files in the binary

--- migrations/fs.go
+++ migrations/fs.go
@@ -4,3 +4,6 @@ import "embed"
 
 //go:embed *.sql
 var FS embed.FS
+
+// any sql files in this directory will be embedded into the binary.
+// This makes it much easier to run migrations anywhere without also copying the .sql files around.
