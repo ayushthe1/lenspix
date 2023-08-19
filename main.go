a39a8166d0c5c39c591d0e9aa94b227aa9dd85b9
ayushthe1 <ayushsharmaa101@gmail.com>
2023-08-19 18:52:26 +0530

    refractor and and middleware to /users/me routes

--- main.go
+++ main.go
@@ -47,24 +47,7 @@ import (
 
 func main() {
 
-	r := chi.NewRouter()
-
-	// parse the template
-	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
-
-	r.Get("/", controllers.StaticHandler(tpl))
-	// r.Get("/", http.HandlerFunc(homeHandler))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
-	r.Get("/contact", controllers.StaticHandler(tpl))
-
-	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
-	r.Get("/faq", controllers.FAQ(tpl))
-
-	// tpl = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
-	// r.Get("/signup", controllers.StaticHandler(tpl))
-
-	// get the database connection
+	// Setup the database connection
 	cfg := models.DefaultPostgresConfig()
 	fmt.Println(cfg.String())
 	db, err := models.Open(cfg)
@@ -81,17 +64,28 @@ func main() {
 		panic(err)
 	}
 
+	// Setup services
 	// Setup our model services
 	userService := models.UserService{
 		DB: db,
 	}
-
 	// setup our user service
 	sessionService := models.SessionService{
 		DB: db,
 	}
 
-	// Setup our controllers
+	// Setup middleware
+	umw := controllers.UserMiddleware{
+		SessionService: &sessionService,
+	}
+	csrfKey := "Wc0gT1xfFAjlRwip7l7MmEdjw7DzMXamEHLjyAUP"
+	csrfMw := csrf.Protect(
+		[]byte(csrfKey),
+		//TODO: Fix this before deploying
+		csrf.Secure(false),
+	)
+
+	// Setup controllers
 	userC := controllers.Users{
 		UserService:    &userService,
 		SessionService: &sessionService,
@@ -99,28 +93,56 @@ func main() {
 	userC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	userC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
 
+	// Setup our router and routes
+
+	r := chi.NewRouter()
+	// Applying the niddleware
+	// These middleware run on all the incoming request
+	r.Use(csrfMw)
+	r.Use(umw.SetUser)
+
+	// parse the template
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))
+
+	r.Get("/", controllers.StaticHandler(tpl))
+	// r.Get("/", http.HandlerFunc(homeHandler))
+
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))
+	r.Get("/faq", controllers.FAQ(tpl))
+
+	// tpl = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	// r.Get("/signup", controllers.StaticHandler(tpl))
+
 	r.Get("/signup", userC.New)
 	r.Get("/signin", userC.SignIn)
-	r.Post("/users", userC.Create)
+	r.Post("/signup", userC.Create)
 	r.Post("/signin", userC.ProcessSignIn)
-	r.Get("/users/me", userC.CurrentUser)
 	r.Post("/signout", userC.ProcessSignOut)
+	r.Get("/users/me", userC.CurrentUser)
+	// r.Get("/users/me", userC.CurrentUser) --> before
+
+	// Apply the router to all routes that match the prefix 'users/me'
+	r.Route("/users/me", func(r chi.Router) {
+		// RequireUser middleware will be used on all routes with the /users/me prefix
+		r.Use(umw.RequireUser)
+		r.Get("/", userC.CurrentUser)
+		r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hellooo")
+		})
+	})
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 	})
 
+	// Wrapping csrfMw as a middleware around r and starting the server
 	fmt.Println("Starting the server on port :3000 ......")
-
-	csrfKey := "Wc0gT1xfFAjlRwip7l7MmEdjw7DzMXamEHLjyAUP"
-
-	csrfMw := csrf.Protect(
-		[]byte(csrfKey),
-		//TODO: Fix this before deploying
-		csrf.Secure(false),
-	)
-	// Wrapping csrfMw as a middleware around r.
-	http.ListenAndServe(":3000", csrfMw(r))
+	http.ListenAndServe(":3000", r)
+	// http.ListenAndServe(":3000", csrfMw(umw.SetUser(r)))
+	// In this code we are wrapping the router with the middleware that sets a user, then we wrap that whole http handler with the CSRF middleware. This means CSRF protection will run first, then our user lookup, and finally our router will decide what HTTP handler to use based on the path and HTTP method. The order of these can be important.
 }
 
 // timer middleware to know the response time for our requests
