9eeb84ec70d7ea96b87035d8b1dfa2a381d226ba
ayushthe1 <ayushsharmaa101@gmail.com>
2023-08-23 22:05:13 +0530

    add functions forconsuming and creating passwordReset

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/ayushthe1/lenspix/rand"
 )
 
 type PasswordReset struct {
@@ -34,11 +39,103 @@ type PasswordResetService struct {
 }
 
 func (service *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: Complete the function")
+	// Verify we have a valid email address for a user, and get that user's ID
+	email = strings.ToLower(email)
+	var userID int
+	row := service.DB.QueryRow(`
+	SELECT id FROM users WHERE email = $1;`, email)
+	err := row.Scan(&userID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	// Build the passwordReset
+	bytesPerToken := service.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	duration := service.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserID:    userID,
+		Token:     token,
+		Tokenhash: service.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	// Insert the passwordreset into the db
+	row = service.DB.QueryRow(`
+	INSERT INTO password_resets (user_id, token_hash, expires_at)
+	VALUES ($1, $2, $3) ON CONFLICT (user_id) DO
+	UPDATE
+	SET token_hash = $2, expires_at=$3
+	RETURNING id;`, pwReset.UserID, pwReset.Tokenhash, pwReset.ExpiresAt)
+
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	return &pwReset, nil
+
 }
 
 // function to take an exisiting password reset token and to use it
 // It will consume a token and return the user associated with it, or return an error if the token wasn't valid for any reason. Later when the password reset token is consumed ,we can call the userService to update the user
 func (service *PasswordResetService) Consume(token string) (*User, error) {
-	return nil, fmt.Errorf("TODO")
+	tokenHash := service.hash(token)
+	var user User
+	var pwReset PasswordReset
+	// lookup both the password reset token and its associated user in a single query
+	row := service.DB.QueryRow(`
+		SELECT password_resets.id,
+			password_resets.expires_at,
+			users.id,
+			users.email,
+			users.password_hash
+		FROM password_resets
+			JOIN users ON users.id = password_resets.user_id
+		WHERE password_resets.token_hash = $1;`, tokenHash)
+	err := row.Scan(
+		&pwReset.ID, &pwReset.ExpiresAt,
+		&user.ID, &user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+
+	// check if the passwordReset is not expired
+	if time.Now().After(pwReset.ExpiresAt) {
+		return nil, fmt.Errorf("token expired: %v", token)
+	}
+
+	// delete the passwordReset
+	err = service.delete(pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+	return &user, nil
+}
+
+// function for hashing the token
+func (service *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
+}
+
+// function to delete the passwordReset from DB
+func (service *PasswordResetService) delete(id int) error {
+	_, err := service.DB.Exec(`
+		DELETE FROM password_resets
+		WHERE id = $1;`, id)
+	if err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+	return nil
 }
