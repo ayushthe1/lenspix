7a09d243a4ddea4a283ee138e6dfd9ec70db627e
ayushthe1 <ayushsharmaa101@gmail.com>
2023-08-28 23:11:49 +0530

    add services for managing galleries

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,118 @@
 package models
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
 type Gallery struct {
 	ID     int
 	UserID int
 	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+// service to create a gallery
+func (service *GalleryService) Create(title string, userID int) (*Gallery, error) {
+	// define the gallery object
+	gallery := Gallery{
+		Title:  title,
+		UserID: userID,
+	}
+	row := service.DB.QueryRow(`
+	INSERT INTO galleries (title, user_id)
+	VALUES ($1, $2) RETURNING id;`, gallery.Title, gallery.UserID)
+
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+
+	return &gallery, nil
+
+}
+
+// service to query gallery by id
+func (service *GalleryService) ByID(id int) (*Gallery, error) {
+	gallery := Gallery{
+		ID: id,
+	}
+
+	row := service.DB.QueryRow(`
+	SELECT title, user_id
+	FROM galleries
+	WHERE id = $1;`, gallery.ID)
+
+	err := row.Scan(&gallery.Title, &gallery.UserID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound //  users of the models package don’t need to know about sql being used
+		}
+
+		return nil, fmt.Errorf("query gallery by id: %w", err)
+	}
+
+	return &gallery, nil
+
+}
+
+// service to query all galleries associated with a user
+func (service *GalleryService) ByUserID(userID int) ([]Gallery, error) {
+	rows, err := service.DB.Query(`
+	SELECT id, title
+	FROM galleries
+	WHERE user_id = $1;`, userID)
+
+	if err != nil {
+		return nil, fmt.Errorf("query galleries by user: %w", err)
+	}
+
+	var galleries []Gallery
+	for rows.Next() {
+		gallery := Gallery{
+			UserID: userID,
+		}
+		err = rows.Scan(&gallery.ID, &gallery.Title)
+		if err != nil {
+			return nil, fmt.Errorf("query galleries by user: %w", err)
+		}
+		// add the gallery to our galleries slice
+		galleries = append(galleries, gallery)
+	}
+
+	if rows.Err() != nil {
+		return nil, fmt.Errorf("query galleries by user: %w", err)
+	}
+	return galleries, nil
+}
+
+func (service *GalleryService) Update(gallery *Gallery) error {
+	// we're using exec instead of Query as we son't care about the return values
+	// update the title of the gallery
+	_, err := service.DB.Exec(`
+	UPDATE galleries
+	SET title = $2
+	WHERE id = $1;
+	`, gallery.ID, gallery.Title)
+	if err != nil {
+		return fmt.Errorf("update gallery: %w", err)
+	}
+	return nil
+
+}
+
+// service to delete a gallery
+func (service *GalleryService) Delete(id int) error {
+
+	_, err := service.DB.Exec(`
+	DELETE FROM galleries
+	WHERE id = $1 ;`, id)
+	if err != nil {
+		return fmt.Errorf("delete gallery: %w", err)
+	}
+	return nil
 }
