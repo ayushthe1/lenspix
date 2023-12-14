84efc75d257ae111eff351848aca3e155361031e
ayushthe1 <ayushsharmaa101@gmail.com>
2023-12-14 11:08:34 +0530

    build tailwind locally

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/**/*.{gohtml,html}" // include files in templates dir and its subdir having extension gohtml & html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
