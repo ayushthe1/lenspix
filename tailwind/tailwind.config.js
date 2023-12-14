eb20e9d08bf19c0110234e289abbf51140ee15d8
ayushthe1 <ayushsharmaa101@gmail.com>
2023-12-14 19:38:51 +0530

    setup tailwind css via docker

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../templates/**/*.{gohtml,html}" // include files in templates dir and its subdir having extension gohtml & html
+    // path to templates dir inside docker container. When setting up locally, we will need to cange the path.
+    "/templates/**/*.{gohtml,html}" // include files in templates dir and its subdir having extension gohtml & html
   ],
   theme: {
     extend: {},
